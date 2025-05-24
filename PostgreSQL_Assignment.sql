4c5320d08b8fb15cf2d86b704f68f16fb12ed5a0
hayder <hayderbd4290@gmail.com>
2025-05-24 21:10:35 +0600

    create species sighting table

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,4 +1,5 @@
--- Active: 1747477117878@@127.0.0.1@5432@conservation_db@public
+-- Active: 1748099194238@@127.0.0.1@5432@conservation_db@public
+
 
 -- create new conservation_db database
 CREATE DATABASE conservation_db;
@@ -8,4 +9,25 @@ CREATE TABLE rangers (
     ranger_id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     region VARCHAR(80) NOT NULL
+);
+
+
+-- create species table
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(100) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(50) CHECK (conservation_status IN ('Endangered', 'Vulnerable', 'Historic'))
+);
+
+
+-- create sightings table
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    species_id INT REFERENCES species(species_id) ON DELETE CASCADE, 
+    ranger_id INT REFERENCES rangers(ranger_id) ON DELETE CASCADE, 
+    sighting_time TIMESTAMP NOT NULL,
+    location VARCHAR(150) NOT NULL,
+    notes TEXT
 );
