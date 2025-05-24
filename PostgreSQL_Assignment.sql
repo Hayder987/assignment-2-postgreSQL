03c1cdef9e0256128319f2c7e25e65b7e7c25dae
hayder <hayderbd4290@gmail.com>
2025-05-24 22:13:40 +0600

    insert data on table rangers, species, sighting

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -31,3 +31,43 @@ CREATE TABLE sightings (
     location VARCHAR(150) NOT NULL,
     notes TEXT
 );
+
+
+-- insert data on table rangers
+INSERT INTO rangers (name, region) VALUES
+('Alice Green', 'Northern Hills'),
+('Bob White', 'River Delta'),
+('Carol King', 'Mountain Range'),
+('Derek Fox', 'Coastal Plains'),
+('Ella Stone', 'Desert Edge'),
+('Frank River', 'Wetland Marshes'),
+('Grace Bloom', 'Forest Zone');
+
+
+-- insert data on TAble species
+INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status) VALUES
+('Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+('Dodo', 'Raphus cucullatus', '1598-01-01', 'Historic'),
+('Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+('Indian Pangolin', 'Manis crassicaudata', '1822-01-01', 'Vulnerable'),
+('Passenger Pigeon', 'Ectopistes migratorius', '1800-01-01', 'Historic'),
+('Golden Langur', 'Trachypithecus geei', '1956-03-15', 'Endangered');
+
+
+-- insert data on Table sightings
+INSERT INTO sightings (species_id, ranger_id, sighting_time, location, notes) VALUES
+(1, 1, '2024-05-10 07:45:00', 'Peak Ridge', 'Camera trap image captured'),
+(4, 2, '2024-05-12 16:20:00', 'Bankwood Area', 'Juvenile seen near riverbank'),
+(2, 3, '2024-05-15 09:10:00', 'Bamboo Grove East', 'Feeding observed in dense cover'),
+(1, 2, '2024-05-18 18:30:00', 'Snowfall Pass', NULL),
+(5, 4, '2024-05-20 06:30:00', 'Rocky Trail', 'Seen burrowing near rocks'),
+(7, 6, '2024-05-21 19:15:00', 'River Cliff', 'Heard calls, no visuals'),
+(6, 5, '2024-05-23 11:00:00', 'Pine Meadow', 'Bones discovered, possible old sighting');
+
+
+-- display all table data
+SELECT * FROM rangers;
+SELECT * FROM species;
+SELECT * FROM sightings;
+
